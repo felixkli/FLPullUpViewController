0211cc7e55caf42e68cae4d40aa775be009a0b53
Felix Li <li.felix162@gmail.com>
2020-09-30 16:42:15 +0800

    0.5.21 - Add max height - Add tap to close flag

--- FLPullUpViewController.swift
+++ FLPullUpViewController.swift
@@ -51,7 +51,7 @@ private class ContainerVC: UIViewController {
 
 
     // MARK: Life Cycle
-        
+    
     override public func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
@@ -78,17 +78,18 @@ private class ContainerVC: UIViewController {
         
         if let navVC = rootViewController as? UINavigationController {
             
-            navVC.navigationBar.frame = CGRect(x: navVC.navigationBar.frame.origin.x, y: pullBarHeight, width: containerWidth, height: 44)
+            navVC.navigationBar.frame = CGRect(x: navVC.navigationBar.frame.origin.x, y: pullBarHeight, width: navVC.navigationBar.frame.width, height: 44)
+            
             navVC.topViewController?.edgesForExtendedLayout = [.top, .bottom]
         }
         
         self.darkScreenView.frame = view.bounds
+        
         self.pullTabContainerView.frame = CGRect(x: containerX, y: -20, width: containerWidth, height: pullBarHeight)
         
-        if self.containerView.frame == CGRect.zero  {
+        if self.containerView.frame == CGRect.zero {
             
             self.containerView.frame = CGRect(x: containerX, y: view.bounds.height, width: containerWidth, height: view.bounds.height)
-            self.rootViewController.view.frame = self.containerView.bounds
             self.darkScreenView.updateFrame()
         }
         
@@ -103,7 +104,6 @@ private class ContainerVC: UIViewController {
                 
                 navVC.navigationBar.addSubview(self.pullTabContainerView)
             }
-            //            self.containerView.addSubview(self.pullTabContainerView)
             
             self.containerView.addSubview(self.pullTabImageView)
         }else{
@@ -115,14 +115,14 @@ private class ContainerVC: UIViewController {
             
             self.containerView.frame.origin = CGPoint(x: containerX, y: self.view.frame.height - self.pullUpDistance)
             self.containerView.frame.size = CGSize(width: containerWidth, height: max(self.pullUpDistance, (self.originalPullDistance ?? 0)))
-            
+                        
             if #available(iOS 11.0, *) {
                 self.rootViewController.additionalSafeAreaInsets = UIEdgeInsets(top: pullBarHeight, left: 0, bottom: 0, right: 0)
             }
-            
+                        
             // Setting rootViewController as frame
             self.rootViewController.view.frame = CGRect(x: 0, y: 0, width: self.containerView.bounds.width, height: self.containerView.frame.height)
-            
+ 
             self.darkScreenView.updateFrame()
             
         }) { complete in
@@ -143,7 +143,7 @@ private class ContainerVC: UIViewController {
             }
         }
     }
-    
+   
     public override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         
@@ -180,6 +180,15 @@ public class FLPullUpViewController {
     
     // flags
     public var useSystemLayoutSizeFitting: Bool = false
+    public var tapToClose: Bool = true {
+        didSet{
+            if tapToClose {
+                self.viewController.darkScreenView.addGestureRecognizer(tapGesture)
+            }else if self.viewController.darkScreenView.gestureRecognizers?.contains(tapGesture) == true{
+                self.viewController.darkScreenView.removeGestureRecognizer(tapGesture)
+            }
+        }
+    }
     public var pullToClose: Bool = true {
         didSet{
             if pullToClose {
@@ -217,7 +226,6 @@ public class FLPullUpViewController {
             }
         }
     }
-    
     
     private var blurBackground = false {
         didSet{
@@ -277,11 +285,10 @@ public class FLPullUpViewController {
                 // Need to call again in order to set view size to update correctly when pull distance has not changed
                 intrinsicSizeVC.view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
             }
-            
-            self.viewController.pullUpDistance = newDistance
+                
+            self.viewController.pullUpDistance = min(self.viewController.view.bounds.height - 40, newDistance)
             if originalPullDistance == nil {
                 
-                print("[pull] set originalPullDistance: \(self.pullUpDistance)")
                 self.originalPullDistance = self.pullUpDistance
             }
             viewController.view.setNeedsLayout()
@@ -339,6 +346,7 @@ public class FLPullUpViewController {
             }else{
                 self.viewController.pullTabContainerView.backgroundColor = UIColor.clear
             }
+            
             self.viewController.addChild(child: rootViewController, to: self.viewController.containerView)
         }
     }
@@ -359,6 +367,11 @@ public class FLPullUpViewController {
             }else{
                 self.viewController.pullTabContainerView.backgroundColor = UIColor.clear
             }
+
+//            navVC.navigationBar.backgroundColor = UIColor.clear
+//            navVC.view.backgroundColor = UIColor.clear
+            
+//            viewController.view.backgroundColor = navVC.navigationBar.backgroundColor
         }
         
         updateContainer()
@@ -381,9 +394,12 @@ public class FLPullUpViewController {
             self.viewController.containerView.addGestureRecognizer(panGesture)
         }
         
+        if tapToClose {
+            viewController.darkScreenView.addGestureRecognizer(tapGesture)
+        }
+
         // initial frame to animate from
         viewController.darkScreenView.frame = viewController.view.bounds
-        viewController.darkScreenView.addGestureRecognizer(tapGesture)
         viewController.darkScreenView.hide = true
         viewController.darkScreenView.maskedView = self.viewController.containerView
         
@@ -513,7 +529,7 @@ public class FLPullUpViewController {
             if pullUpDistance > max(originalPullDistance ?? 0, screenRatio * viewController.view.bounds.height) {
                 pullUpDistance = max(originalPullDistance ?? 0, screenRatio * viewController.view.bounds.height)
             }
-                        
+            
         case .ended, .cancelled, .failed, .possible:
             
             UIView.setAnimationsEnabled(true)
